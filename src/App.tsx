110d154d2c0bb8a103745efa44193984b1d085ac
coz <codesver@gmail.com>
2025-02-10 20:51:45 +0900

    feat : apply theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import AppRouter from "@routers/AppRouter";
 
+import { ThemeProvider } from "@mui/material";
+import GlobalTheme from "@assets/themes/GlobalTheme";
+
 import "./App.css";
 
 const App = () => {
-  return <AppRouter />;
+  return (
+    <ThemeProvider theme={GlobalTheme}>
+      <AppRouter />
+    </ThemeProvider>
+  );
 };
 
 export default App;
