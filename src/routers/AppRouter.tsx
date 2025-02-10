d955e5ced7061367de3e2fd2c82af2e908eced0a
coz <codesver@gmail.com>
2025-02-10 09:23:51 +0900

    fix : routing 방식 오류 수정

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,3 +1,4 @@
+import ComponentPage from "@pages/dev/component/ComponentPage";
 import LoginPage from "@pages/login/LoginPage";
 import AuthRouter from "@routers/AuthRouter";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -6,9 +7,9 @@ const AppRouter = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<LoginPage />} />
-      <AuthRouter>
-        {/* Route pages which authentication is necessary */}
-      </AuthRouter>
+      <Route element={<AuthRouter />}>
+        <Route path="/dev/component" element={<ComponentPage />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
