6cb36f8e6648319c045032059c02f239e3e62a66
coz <codesver@gmail.com>
2025-02-22 13:32:13 +0900

    feat : add unauthenticated router
    - Routing inside UnAuthRouter is only allowed for unauthenticated clients.
    - Authenticated users will be redirected to /

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,24 +1,10 @@
-import { Navigate } from "react-router-dom";
-
 import { Box } from "@mui/material";
 
 import styled from "styled-components";
 
-import useAuthValidQuery from "@apis/auth/hooks/useAuthValidQuery";
-
 import LoginForm from "@pages/login/_component/login-form/LoginForm";
 
 const LoginPage = () => {
-  const { isLoading, isSuccess } = useAuthValidQuery();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isSuccess) {
-    return <Navigate to="/" />;
-  }
-
   return (
     <LoginPageContainer>
       <LoginForm />
--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -3,11 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "@pages/login/LoginPage";
 
 import AuthRouter from "@routers/AuthRouter";
+import UnAuthRouter from "@routers/UnAuthRouter";
 
 const AppRouter = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
+      <Route element={<UnAuthRouter />}>
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
       <Route element={<AuthRouter />}>
         <Route path="/" />
       </Route>
