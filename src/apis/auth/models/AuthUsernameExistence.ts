6fd50bd422eee8f356babab800ead304297713dd
coz <codesver@gmail.com>
2025-03-04 23:00:31 +0900

    feat : remove username existence check

--- src/apis/auth/models/AuthUsernameExistence.ts
+++
@@ -1,3 +0,0 @@
-export interface AuthUsernameExistenceRequest {
-  username: string;
-}
--- src/pages/join/_component/controllers/JoinUsernameController.tsx
+++ src/pages/join/_component/controllers/JoinUsernameController.tsx
@@ -1,43 +1,13 @@
-import { useEffect } from "react";
-
 import { Controller, useFormContext } from "react-hook-form";
 
-import { Box, Button, TextField } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 
 import styled from "styled-components";
 
-import useAuthUsernameExistenceQuery from "@apis/auth/hooks/useAuthUsernameExistenceQuery";
-
 import JoinFormSchema from "@pages/join/_schema/JoinFormSchema";
 
-import useToastStore from "@stores/useToastStore";
-
 const JoinUsernameController = () => {
-  const { showInfo, showWarning, showError } = useToastStore();
-
-  const { control, watch } = useFormContext<JoinFormSchema>();
-
-  const enteredUsername = watch("username");
-  const { data: usernameExist, refetch: validateUsernameDuplication } =
-    useAuthUsernameExistenceQuery({
-      username: enteredUsername,
-    });
-
-  const handleCheck = () => {
-    if (enteredUsername) {
-      validateUsernameDuplication();
-    } else {
-      showWarning("Please enter a username");
-    }
-  };
-
-  useEffect(() => {
-    if (usernameExist) {
-      showError("Username already exists");
-    } else if (usernameExist === false) {
-      showInfo(`Username ${enteredUsername} is not used`);
-    }
-  }, [usernameExist]);
+  const { control } = useFormContext<JoinFormSchema>();
 
   return (
     <Controller
@@ -49,9 +19,8 @@ const JoinUsernameController = () => {
             type="text"
             placeholder="Username"
             onChange={field.onChange}
-            error={!!error || usernameExist}
+            error={!!error}
           />
-          <UsernameCheckButton onClick={handleCheck}>Check</UsernameCheckButton>
         </JoinUsernameContainer>
       )}
     />
@@ -69,7 +38,5 @@ const JoinUsernameContainer = styled(Box)`
 const UsernameInput = styled(TextField)`
   width: 100%;
 `;
-
-const UsernameCheckButton = styled(Button)``;
 
 export default JoinUsernameController;
