256cc3237c48559d9ad515528521166609a7e14b
coz <codesver@gmail.com>
2025-04-23 23:02:45 +0900

    refactor : split loading store's state and action

--- src/stores/useLoadingStore.ts
+++ src/stores/useLoadingStore.ts
@@ -4,9 +4,11 @@ interface LoadingState {
   isLoading: boolean;
 }
 
-interface LoadingStore extends LoadingState {
+interface LoadingAction {
   setLoading: (isLoading: boolean) => void;
 }
+
+type LoadingStore = LoadingState & LoadingAction;
 
 const useLoadingStore = create<LoadingStore>((set) => ({
   isLoading: false,
