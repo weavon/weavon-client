8a0e3112afcd5d0eceefab6566a3f9016c77a7b6
coz <codesver@gmail.com>
2025-06-11 01:05:57 +0900

    chore : configure eslint of react

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
@@ -14,17 +15,32 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    settings: {
+      react: {
+        version: "19",
+      },
+    },
     plugins: {
+      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       import: importPlugin,
     },
     rules: {
+      ...react.configs.recommended.rules,
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       ...reactRefresh.configs.recommended.rules,
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/function-component-definition": [
+        "error",
+        {
+          namedComponents: ["function-declaration", "function-expression"],
+          unnamedComponents: ["function-expression", "function-expression"],
+        },
+      ],
       "import/order": [
         "error",
         {
