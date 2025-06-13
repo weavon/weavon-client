f8b837196e90f7d5a1b83a7090a021fab79a8af9
coz <codesver@gmail.com>
2025-06-13 23:05:33 +0900

    feat : clear auth store when unauth

--- src/components/guard/UnAuthGuard.tsx
+++ src/components/guard/UnAuthGuard.tsx
@@ -3,13 +3,21 @@ import { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 import useAuthValidQuery from "@/apis/auth/queries/useAuthValidQuery";
+import useAuthStore from "@/stores/useAuthStore";
 import useLoadingStore from "@/stores/useLoadingStore";
 
 function UnAuthGuard() {
   const { setLoading } = useLoadingStore();
+  const { logout } = useAuthStore();
 
   const { isSuccess: isAuthValid, isLoading: isAuthValidLoading } =
     useAuthValidQuery();
+
+  useEffect(() => {
+    if (!isAuthValid) {
+      logout();
+    }
+  }, [isAuthValid, logout]);
 
   useEffect(() => {
     setLoading(isAuthValidLoading);
